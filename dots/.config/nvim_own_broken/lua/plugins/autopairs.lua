a9ab33d1b474bf702bcea9d87c71b72068ac2a36
MaximilianTroester <maximilian.troe@gmail.com>
2024-08-19 19:48:51 +0200

    updated rofi config

--- dots/.config/nvim_own_broken/lua/plugins/autopairs.lua
+++
@@ -1,6 +0,0 @@
-return {
-    'windwp/nvim-autopairs',
-    events = "InsertEnter",
-    opts ={}
-}
-
--- dots/.config/nvim_own_broken/lua/plugins/undotree.lua
+++
@@ -1,6 +0,0 @@
-return  {
-    'mbbill/undotree',
-    config = function ()
-        vim.keymap.set('n', '<Leader>u', vim.cmd.UndotreeToggle, { silent = true })
-    end
-}
