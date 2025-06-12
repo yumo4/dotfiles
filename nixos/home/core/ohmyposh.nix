3be000432c6a91e272fe162f377f015eaa7be2d0
yumo4 <maximilian.troe@gmail.com>
2025-06-12 17:29:13 +0200

    test

--- nixos/home/core/ohmyposh.nix
+++ nixos/home/core/ohmyposh.nix
@@ -1,3 +1,84 @@
+# {
+#   self,
+#   inputs,
+#   ...
+# }: {
+#   programs.oh-my-posh = {
+#     enable = true;
+#     enableZshIntegration = true;
+#     enableFishIntegration = true;
+#     # settings = builtins.fromTOML (builtins.unsafeDiscardStringContext (builtins.readFile inputs.self + "../../../dots/.config/ohmyposh/config.toml"));
+#     settings = {
+#       console_title_template = "{{ .Shell }} in {{ .Folder }}";
+#       version = 3;
+#       final_space = true;
+#
+#       secondary_prompt = {
+#         template = "➜➜";
+#         foreground = "yellow";
+#         background = "transparent";
+#       };
+#
+#       transient_prompt = {
+#         template = "➜ ";
+#         background = "transparent";
+#         foreground_templates = [
+#           "{{if gt .Code 0}}red{{end}}"
+#           "{{if eq .Code 0}}yellow{{end}}"
+#         ];
+#       };
+#
+#       blocks = [
+#         {
+#           type = "prompt";
+#           alignment = "left";
+#           newline = true;
+#           segments = [
+#             {
+#               template = "{{ .Path }}";
+#               foreground = "cyan";
+#               background = "transparent";
+#               type = "path";
+#               style = "plain";
+#               properties = {
+#                 style = "full";
+#               };
+#             }
+#             {
+#               template = " {{ .HEAD }}{{ if or (.Working.Changed) (.Staging.Changed) }}<yellow>*</>{{ end }} <cyan>{{ if gt .Behind 0 }}⇣{{ end }}{{ if gt .Ahead 0 }}⇡{{ end }}</>";
+#               foreground = "p:grey";
+#               background = "transparent";
+#               type = "git";
+#               style = "plain";
+#               properties = {
+#                 branch_icon = "";
+#                 commit_icon = "@";
+#                 fetch_status = true;
+#               };
+#             }
+#           ];
+#         }
+#         {
+#           type = "prompt";
+#           alignment = "left";
+#           newline = true;
+#           segments = [
+#             {
+#               template = "➜";
+#               background = "transparent";
+#               type = "text";
+#               style = "plain";
+#               foreground_templates = [
+#                 "{{if gt .Code 0}}red{{end}}"
+#                 "{{if eq .Code 0}}yellow{{end}}"
+#               ];
+#             }
+#           ];
+#         }
+#       ];
+#     };
+#   };
+# }
 {
   self,
   inputs,
@@ -28,6 +109,13 @@
         ];
       };
 
+      # Add right-side transient prompt for SSH indicator
+      rtransient_prompt = {
+        template = "{{ if .Env.SSH_CONNECTION }}<magenta>ssh:</><cyan>{{ .Env.SSH_CLIENT | regexFind \"^[^\\s]+\" }}</>{{ end }}";
+        background = "transparent";
+        foreground = "cyan";
+      };
+
       blocks = [
         {
           type = "prompt";
@@ -55,6 +143,20 @@
                 commit_icon = "@";
                 fetch_status = true;
               };
+            }
+          ];
+        }
+        # Add right-aligned block for SSH indicator in normal prompt
+        {
+          type = "prompt";
+          alignment = "right";
+          segments = [
+            {
+              template = "{{ if .Env.SSH_CONNECTION }}<magenta>ssh:</><cyan>{{ .Env.SSH_CLIENT | regexFind \"^[^\\s]+\" }}</>{{ end }}";
+              background = "transparent";
+              type = "session";
+              style = "plain";
+              foreground = "cyan";
             }
           ];
         }
