c579fe6de3c5cd974c96572d4d1475048d7def3a
yumo4 <maximilian.troe@gmail.com>
2025-07-08 15:34:51 +0200

    framework

--- nixos/machines/framework/home.nix
+++ nixos/machines/framework/home.nix
@@ -5,6 +5,7 @@
     ../../home/optional/ghostty.nix
     ../../home/optional/hyprland.nix
     ../../home/optional/hyprlock.nix
+    ../../home/optional/niri
     ../../home/optional/sops.nix
     ../../home/optional/ssh.nix
     ../../home/optional/syncthing.nix
