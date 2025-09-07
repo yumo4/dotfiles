359b0beaf938393f55dfeb9e98ae845298ac532b
yumo4 <maximilian.troe@gmail.com>
2025-09-07 12:48:08 +0200

    updated "normal" fish-config

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -1,4 +1,4 @@
-if status is-interactive
+# if status is-interactive
 #    # Commands to run in interactive sessions can go here
 alias vim "nvim"
 alias fetch "fastfetch"
@@ -6,20 +6,36 @@ alias ll "lsd -l"
 alias ls "lsd"
 alias lst "tree -C"
 alias tas="tmux attach-session -t"
-alias FHWS "fortivpn connect FHWS -u k61965 -p -s"
-alias home-one "ssh max@192.168.178.65"
-alias homec="sudo wg-quick up wg_config"
-alias homedc="sudo wg-quick down wg_config"
-
-set -g fish_greeting ""
 
 oh-my-posh init fish --config $HOME/.config/ohmyposh/config.toml | source
 end
 
-export PATH=$PATH:$(go env GOPATH)/bin
+# export PATH=$PATH:$(go env GOPATH)/bin
+#
+# if [ -e /home/max/.nix-profile/etc/profile.d/nix.sh ]; then . /home/max/.nix-profile/etc/profile.d/nix.sh; 
+# end # added by Nix installer
 
-if [ -e /home/max/.nix-profile/etc/profile.d/nix.sh ]; then . /home/max/.nix-profile/etc/profile.d/nix.sh; 
-end # added by Nix installer
+# Enable vi key bindings
+fish_vi_key_bindings
 
-# Generated for envman. Do not edit.
-#test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
+# Configure FZF completion
+set -U FZF_COMPLETE 0
+set -U FZF_DEFAULT_OPTS "--bind 'ctrl-y:accept'"
+
+set -g fish_greeting "" 
+set fish_cursor_default block
+set fish_cursor_insert block
+set fish_color_autosuggestion brblack
+
+function fish_mode_prompt
+    # No body, just an empty function
+end
+
+function fish_user_key_bindings
+    # Insert mode bindings
+    bind --mode insert ctrl-y accept-autosuggestion
+    bind --mode insert ctrl-n down-or-search
+    bind --mode insert ctrl-p up-or-search
+end
+
+zoxide init fish | source
