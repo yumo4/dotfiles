f70d7a93b2cb1f660b42ee427c3108798dec7e8e
yumo4 <maximilian.troe@gmail.com>
2025-10-06 18:39:39 +0200

    chore(nvim): updated blink-cmp config to use luasnip

--- dots/.config/nvim/lua/plugins/blink-cmp.lua
+++ dots/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,6 +1,31 @@
 return {
   "saghen/blink.cmp",
-  dependencies = "rafamadriz/friendly-snippets",
+  dependencies = {
+    {
+      "L3MON4D3/LuaSnip",
+      version = "2.*",
+      build = (function()
+        -- Build Step is needed for regex support in snippets.
+        -- This step is not supported in many windows environments.
+        -- Remove the below condition to re-enable on windows.
+        if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+          return
+        end
+        return "make install_jsregexp"
+      end)(),
+      dependencies = {
+        -- `friendly-snippets` contains a variety of premade snippets.
+        {
+          "rafamadriz/friendly-snippets",
+          config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+          end,
+        },
+      },
+      opts = {},
+    },
+    "folke/lazydev.nvim",
+  },
 
   version = "v1.*",
   build = "nix run .#build-plugin",
