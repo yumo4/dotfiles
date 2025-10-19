9063628b3e326dd1a02a0f0a6e5ab5c9de97ac1d
yumo4 <maximilian.troe@gmail.com>
2025-10-19 11:00:35 +0200

    chore(framework): fixed wallpaper mode

--- scripts/wallpaper-sway.sh
+++ scripts/wallpaper-sway.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 HOSTNAME=$(hostname)
 if [[ "$HOSTNAME" == "framework" ]]; then
-	swaybg -o '*' -i ~/Projects/dotfiles/wallpaper/friendly-robot-ssd.jpg -m center &
+	swaybg -o '*' -i ~/Projects/dotfiles/wallpaper/friendly-robot-ssd.jpg -m fill &
 elif [[ "$HOSTNAME" == "chimaera" ]]; then
 	swaybg -o '*' -i ~/Projects/dotfiles/wallpaper/julian-calle-falcon.jpg -m center &
 elif [[ "$HOSTNAME" == "lusankya" ]]; then
