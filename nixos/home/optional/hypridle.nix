1af5b4a81b61ce12d15d5e0b9112340cac254221
yumo4 <maximilian.troe@gmail.com>
2025-10-26 11:26:26 +0100

    feat: added hypridle & vicinae

---
+++ nixos/home/optional/hypridle.nix
@@ -0,0 +1,43 @@
+{...}: {
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "niri msg action power-on-monitors";
+        ignore_dbus_inhibit = false;
+        ignore_systemd_inhibit = false;
+      };
+
+      listener = [
+        # Dim brightness after 2.5 minutes
+        # {
+        #   timeout = 150;
+        #   on-timeout = "brightnessctl -s set 10";
+        #   on-resume = "brightnessctl -r";
+        # }
+
+        # Lock screen after 5 minutes
+        {
+          timeout = 300;
+          on-timeout = "loginctl lock-session";
+        }
+
+        # Turn off display
+        # NOTE: niri only
+        {
+          timeout = 320;
+          on-timeout = "niri msg action power-off-monitors";
+          on-resume = "niri msg action power-on-monitors";
+        }
+
+        # Suspend
+        {
+          timeout = 320;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
+}
---
+++ nixos/home/optional/vicinae.nix
@@ -0,0 +1,20 @@
+{config, ...}: {
+  services.vicinae = {
+    enable = true;
+    autoStart = true;
+    # settings = {
+    #   faviconService = "twenty";
+    #   font = {
+    #     size = 11;
+    #     family = "JetBrains Mono";
+    #   };
+    #   theme.name = "gruvbox-dark";
+    #   window = {
+    #     csd = true;
+    #     # opacity = 0.95;
+    #     opacity = 1.00;
+    #     rounding = 10;
+    #   };
+    # };
+  };
+}
