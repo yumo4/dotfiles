360873832e3a498dc6fd702b5e33a65e88a25715
yumo4 <maximilian.troe@gmail.com>
2026-01-06 16:01:49 +0100

    chore: treesitter update

--- dots/.config/nvim/lua/plugins/treesitter.lua
+++ dots/.config/nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,53 @@
 return { -- Highlight, edit, and navigate code
-  "nvim-treesitter/nvim-treesitter",
-  -- branch = "main",
-  build = ":TSUpdate",
-  config = function()
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+  },
+  {
+    "MeanderingProgrammer/treesitter-modules.nvim",
+    config = function()
+      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "bash", "c", "html", "go", "lua", "markdown", "vim", "vimdoc", "latex", "php" },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = false },
-    })
+      require("treesitter-modules").setup({
+        ensure_installed = { "bash", "c", "html", "go", "lua", "markdown", "vim", "vimdoc", "latex", "php" },
+        -- Autoinstall languages that are not installed
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = false },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        select = {
+          enable = true,
+          lookahead = true,
+          selection_modes = {
+            ["@parameter.outer"] = "v",
+            ["@function.outer"] = "V",
+            ["@class.outer"] = "<c-v>",
+          },
+          include_surrounding_whitespace = true,
+        },
+      })
+      local select = require("nvim-treesitter-textobjects.select").select_textobject
 
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  end,
+      vim.keymap.set({ "x", "o" }, "af", function()
+        select("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "if", function()
+        select("@function.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ic", function()
+        select("@class.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "as", function()
+        select("@local.scope", "locals")
+      end)
+    end,
+  },
 }
