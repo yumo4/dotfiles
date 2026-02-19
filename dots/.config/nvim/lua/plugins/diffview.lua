bd4063e5c97d6e05cca1be7502b5b20d65e22a0f
yumo4 <maximilian.troe@gmail.com>
2026-02-19 17:42:01 +0100

    changed keybindings

--- dots/.config/nvim/lua/plugins/diffview.lua
+++ dots/.config/nvim/lua/plugins/diffview.lua
@@ -14,18 +14,90 @@ return {
       },
       keymaps = {
         view = {
-          ["<leader>co"] = actions.conflict_choose("ours"), -- Pick 'ours'
-          ["<leader>ct"] = actions.conflict_choose("theirs"), -- Pick 'theirs"
-          ["<leader>cb"] = actions.conflict_choose("base"), -- Pick 'base'
-          ["<leader>ca"] = actions.conflict_choose("all"), -- Pick both
-          ["<leader>cO"] = actions.conflict_choose("ours"), -- Pick 'ours' for whole file
-          ["<leader>cT"] = actions.conflict_choose("theirs"), -- Pick 'theirs' for whole file
-          ["<leader>dd"] = actions.toggle_files, -- Toggle file panel
+          { "n", "<leader>f", actions.toggle_files, { desc = "toggle files" } },
+          { "n", "<tab>", actions.select_next_entry, { desc = "Open the diff for the next file" } },
+          {
+            "n",
+            "<s-tab>",
+            actions.select_prev_entry,
+            { desc = "Open the diff for the previous file" },
+          },
+          { "n", "<leader>e", actions.focus_files, { desc = "Bring focus to the file panel" } },
+          { "n", "<leader>b", actions.toggle_files, { desc = "Toggle the file panel." } },
+          { "n", "g<C-x>", actions.cycle_layout, { desc = "Cycle through available layouts." } },
+          {
+            "n",
+            "<C-p>",
+            actions.prev_conflict,
+            { desc = "In the merge-tool: jump to the previous conflict" },
+          },
+          {
+            "n",
+            "<C-n>",
+            actions.next_conflict,
+            { desc = "In the merge-tool: jump to the next conflict" },
+          },
+          {
+            "n",
+            "<leader>co",
+            actions.conflict_choose("ours"),
+            { desc = "Choose the OURS version of a conflict" },
+          },
+          {
+            "n",
+            "<leader>ct",
+            actions.conflict_choose("theirs"),
+            { desc = "Choose the THEIRS version of a conflict" },
+          },
+          {
+            "n",
+            "<leader>cb",
+            actions.conflict_choose("base"),
+            { desc = "Choose the BASE version of a conflict" },
+          },
+          {
+            "n",
+            "<leader>ca",
+            actions.conflict_choose("all"),
+            { desc = "Choose all the versions of a conflict" },
+          },
+          { "n", "dx", actions.conflict_choose("none"), { desc = "Delete the conflict region" } },
+          {
+            "n",
+            "<leader>cO",
+            actions.conflict_choose_all("ours"),
+            { desc = "Choose the OURS version of a conflict for the whole file" },
+          },
+          {
+            "n",
+            "<leader>cT",
+            actions.conflict_choose_all("theirs"),
+            { desc = "Choose the THEIRS version of a conflict for the whole file" },
+          },
+          {
+            "n",
+            "<leader>cB",
+            actions.conflict_choose_all("base"),
+            { desc = "Choose the BASE version of a conflict for the whole file" },
+          },
+          {
+            "n",
+            "<leader>cA",
+            actions.conflict_choose_all("all"),
+            { desc = "Choose all the versions of a conflict for the whole file" },
+          },
+          {
+            "n",
+            "dX",
+            actions.conflict_choose_all("none"),
+            { desc = "Delete the conflict region for the whole file" },
+          },
         },
         file_panel = {
-          ["j"] = actions.next_entry,
-          ["k"] = actions.prev_entry,
-          ["<cr>"] = actions.select_entry,
+          { "n", "<leader>f", actions.toggle_files, { desc = "toggle files" } },
+          { "n", "j", actions.next_entry, { desc = "next entry" } },
+          { "n", "k", actions.prev_entry, { desc = "previous entry" } },
+          { "n", "<cr>", actions.select_entry, { desc = "select entry" } },
         },
       },
     })
